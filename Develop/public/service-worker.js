29bb13970a3b2fee8154c55ab4ef3da344efad57
Mariko <mkaku98@gmail.com>
2022-03-19 12:53:49 -0700

    manifest update

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -6,9 +6,7 @@ const FILES_TO_CACHE = [
     "./index.html",
     "./css/styles.css",
     "./js/idb.js",
-    "./js/index.js",
-
-
+    "./js/index.js"
 ];
 
 self.addEventListener('install', function (e) {
